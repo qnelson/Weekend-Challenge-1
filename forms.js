d22f297f2a439c34176d1d0a5ef16e458398410b
Quinn Nelson <quinn.nelson1@gmail.com>
2016-05-02 11:49:57 -0500

    Pro Mode: Monthly Salary calculation now updates when entries are deleted.

--- forms.js
+++ forms.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  var array = [];
+  var empArray = [];
 
   //Prevents browser from contacting server when submit button is clicked
   $('#employeeinfo').on('submit', function (event) {
@@ -13,19 +13,15 @@ $(document).ready(function () {
 
       console.log(values);
 
-      // clear out inputs
+      // clear out inputs and shifts focus to first field
       $('#employeeinfo').find('input[type=text]').val('');
+      $('#employeeinfo').find('input[type=number]').val('');
+      $('#employeefirstname').focus();
 
       //Appends the structure of the HTML page to include newly submitted data
       appendDom(values);
       appendSalary(values);
 
-    });
-
-  //Allows individual entries to be deleted using a button
-  //Deleting an entry will not update the monthly salary calculation
-  $('#container').on('click', 'button', function () {
-      $(this).parent().remove();
     });
 
   //Captures the information entered when the user clicks submit and adds it
@@ -34,7 +30,7 @@ $(document).ready(function () {
     $('#container').append('<div class="person"></div>');
     var $el = $('#container').children().last();
 
-    $el.append('<p>Name: ' + empInfo.employeefirstname + '' + empInfo.employeelastname + '</p>');
+    $el.append('<p>Name: ' + empInfo.employeefirstname + ' ' + empInfo.employeelastname + '</p>');
     $el.append('<p>Employee ID: ' + empInfo.employeenumber + '</p>');
     $el.append('<p>Job Title: ' + empInfo.employeejobtitle + '</p>');
     $el.append('<p>Annual Salary: $' + empInfo.employeesalary + '</p>');
@@ -45,26 +41,34 @@ $(document).ready(function () {
   //Calculates the average monthly salary or
   //(The sum of all entered salaries / 12 months)
   // = Average Monthly Cost of All Salaries
+  var count = 0;
   function appendSalary(empInfo) {
-      $('#monthlysalary').append('<div class="person"></div>');
-      var $el = $('#monthlysalary').children().last();
+    $('#monthlysalary').append('<div class="person"></div>');
+    var $el = $('#monthlysalary').children().last();
 
-      array.push(empInfo.employeesalary);
+    empArray.push(empInfo.employeesalary);
 
-      //Summation function
-      var count = 0;
-      for (var i = array.length; i--;) {
-        count += parseInt(array[i]);
+    //Summation function
+    count = 0;
+    for (var i = empArray.length; i--;) {
+      count += parseInt(empArray[i]);
 
-        //Clears anything in the div everytime the submit button updates the page
-        $('#monthlysalary').children().empty();
-
-        //Average Monthly Salary Cost calculation
-        $el.append('<p>' + '$' + Math.round(count / 12) + '</p>');
-        $('#monthlysalary').children().removeClass();
-
-      }
+      //Average Monthly Salary Cost calculation
+      //.text changes the value in monthlysalary div with every submit
+      $('#title').text('$' + Math.round(count / 12));
+      $('#monthlysalary').children().removeClass();
 
     }
 
+    //Attaches .data tag 'monthlysalary' to empInfo.employeesalary
+    $('.person').last().data('monthlysalary', empInfo.employeesalary);
+  }
+
+  //Allows individual entries to be deleted
+  //Deleted entries will automatically update the Monthly Salary calculation
+  $('#container').on('click', 'button', function () {
+    count -= $('.person').last().data('monthlysalary');
+    $(this).parent().remove();
+    $('#title').text('$' + Math.round(count / 12));
+  });
 });
